ce7b8671f193e73c5537df4a1d8185888c7a5803
hgarr <heriberto.garcia.ribas@gmail.com>
2020-02-16 16:37:47 +0100

    first commit

---
+++ higher_order_1.js
@@ -0,0 +1,21 @@
+const arNumbers = [1,1,2,4,5,32,10,3,7,1,2]
+//count sum of the even numbers that are multiplied by there position in the array
+
+/* const sumEven = arNumbers
+  .filter((number) => number%2===0)
+  .map((number,index)=> number*index)
+  .reduce((num1, num2) => num1+num2);
+*/
+
+  const arEven = arNumbers
+  .filter((number) => number%2===0);
+
+  const arPosition = arEven
+  .map((number,index)=> number*(index+1));
+  
+  const sumEven = arPosition
+  .reduce((num1, num2) => num1+num2);
+
+console.log (arEven)
+console.log (arPosition)
+console.log (sumEven)

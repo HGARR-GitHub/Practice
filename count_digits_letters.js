d14f227edae5b8e65fcc6548b09880dcf5e2cc5f
hgarr <heriberto.garcia.ribas@gmail.com>
2020-02-17 20:43:57 +0100

    add count digits and letters

---
+++ count_digits_letters.js
@@ -0,0 +1,10 @@
+//count the letters and digits in a string
+
+const random = '1ah3jd83j44'
+const arRandom = random.split('')
+
+const letters = arRandom.filter((item)=> isNaN(item)).length
+const digits = arRandom.filter((item)=> !isNaN(item)).length
+console.log ('Count letters: ' + letters)
+console.log ('Count digits: ' + digits)
+

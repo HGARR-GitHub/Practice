7a4da7eb006c7354092fab68fb4309c9afb9e707
hgarr <heriberto.garcia.ribas@gmail.com>
2020-02-28 15:55:48 +0100

    commit, part 2

--- AlphabetPosition.js
+++ AlphabetPosition.js
@@ -8,11 +8,28 @@ function alphabetPosition(text) {
   arrPos = arrText.map((value)=> {
     //Only Map char values that are between 65 and 90 (= A..Z)
     //Everything else just replace with a space
-    return ((value.charCodeAt()-64)>1 & (value.charCodeAt()<91) ? value.charCodeAt()-64 : "")
+    return ((value.charCodeAt()-64)>0 & (value.charCodeAt()<91) ? value.charCodeAt()-64 : "")
   })
-  return arrPos.toString().replace(/,/g, " ")
+  //remove the array items that are ""
+  arrFilter = arrPos.filter((value) => {return value != ""})
+  //convert array to string and remplace the , by space
+  return arrFilter.toString().replace(/,/g, " ")
   
 }
 
+console.log (alphabetPosition("z/az"))
 
-console.log (alphabetPosition("fbca1d&%ef"))
+// Variations:
+// -1-
+// let alphabetPosition = (text) => text.toUpperCase().replace(/[^A-Z]/g, '').split('').map(ch => ch.charCodeAt(0) - 64).join(' ');
+//
+// -2-
+// return text.toLowerCase().replace(/[^a-z]/g,'').split('').map(x => alphabet.indexOf(x)+1).join(' ');
+//
+// -3-
+// const alphabetPosition = text => text
+//  .replace(/[^a-zA-Z]/g, '')
+//  .toUpperCase()
+//  .split``
+//  .map(e => e.charCodeAt() - 64)
+//  .join` `;
